869957535cae1bdb53d3d0dd16acb1b3ae5630b4
Vamshi <vamshivardhanemmadi@gmail.com>
2024-09-12 21:59:16 +0530

    commeting out base

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vamshi-2003/portfolio/',  // Set the base URL for GitHub Pages
+  // base: '/vamshi-2003/portfolio/',  // Set the correct base URL
   plugins: [react()],
 })
