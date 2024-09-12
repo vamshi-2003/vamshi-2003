b9b512bc6086f67809dcc28fd2b9184540c6de4b
Vamshi <vamshivardhanemmadi@gmail.com>
2024-09-12 22:02:18 +0530

    removing sub link

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/vamshi-2003/portfolio/',  // Set the correct base URL
+  base: '/vamshi-2003/',  // Set the correct base URL
   plugins: [react()],
 })
